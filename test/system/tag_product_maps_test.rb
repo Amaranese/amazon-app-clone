0980eae5398af4212aaa5fb9debaaa3b563057d9
Amaranese <alejandro.maranes@asendia.com>
2021-07-24 02:58:32 +0200

    Create tag_product_maps_test.rb

---
+++ test/system/tag_product_maps_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class TagProductMapsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit tag_product_maps_url
+  #
+  #   assert_selector "h1", text: "TagProductMap"
+  # end
+end
